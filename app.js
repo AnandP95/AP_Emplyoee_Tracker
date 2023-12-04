19860f7cf7b2282e01ec1abd71b7979d88bb9530
ANANDNJIT <aplv180@gmail.com>
2023-12-04 17:14:07 -0500

    fix the folder issue

---
+++ app.js
@@ -0,0 +1,69 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2/promise'); // Use mysql2/promise for async/await support
+const figlet = require('figlet');
+const { viewTable, addDepartment, addRole, addEmployee, updateEmployee } = require('./databaseActions');
+
+const choices = [
+  'View all departments',
+  'Add a department',
+  'View all roles',
+  'Add a role',
+  'View all employees',
+  'Add an employee',
+  'Update an employee role',
+];
+
+async function startApp() {
+  try {
+    const db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '590route3',
+      database: 'employeeTracker_db',
+    });
+
+    console.log(`Connected to the employeeTracker_db database.\n`);
+    console.log(figlet.textSync('Employee Tracker'));
+
+    while (true) {
+      const { action } = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: choices,
+      });
+
+      switch (action) {
+        case 'View all departments':
+          await viewTable(db, 'departments');
+          break;
+        case 'Add a department':
+          await addDepartment(db);
+          break;
+        case 'View all roles':
+          await viewTable(db, 'roles');
+          break;
+        case 'Add a role':
+          await addRole(db);
+          break;
+        case 'View all employees':
+          await viewTable(db, 'employees');
+          break;
+        case 'Add an employee':
+          await addEmployee(db);
+          break;
+        case 'Update an employee role':
+          await updateEmployee(db);
+          break;
+        default:
+          console.log('Invalid choice. Please try again.');
+          break;
+      }
+    }
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  }
+}
+
+startApp();
+
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES ("Restaurant"),
+       (" Engineer"),
+       ("Scientists"),
+       ("Mechanic"),
+       ("Farming ");
+
+INSERT INTO  role (title,  salary , department_id)
+VALUES ("Manager", 50000.00, 011),
+       ("Civil", 100000.00,021)
+       ("Pharmacist", 57000.00, 031)
+       ("Worker", 40210.00, 041)
+       ("Farmer", 150790.00, 011);
+       
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Anand", "Patel", 2, 5),
+      ("Anthony", "gujju", 1, 1), 
+      ("Rony", "das", 4, 2), 
+      ("Martin", "Buch", 2, 1), 
+      ("Robert", "Shah", 3, 2);
